19d76253220fff636c301eb075a192ecd11912fe
SnowHana <wujindaniel1011@gmail.com>
2024-03-02 18:55:09 +0900

    only showing me 10 users

--- src/main.py
+++ src/main.py
@@ -1,20 +1,25 @@
 from urllib.parse import quote
-
+from opgg.opgg import OPGG
+from opgg.params import Region
+from opgg.summoner import Summoner
 import requests
 
 API_KEY = "RGAPI-f6591a1d-7714-4b17-87ac-a6b62bea4e2f"
 
 
 class Player:
-    def __init__(self, player_name: str, player_tag: str, region: str) -> None:
+    def __init__(
+        self, player_name: str, player_tag: str, region: str, detailed_region: str
+    ) -> None:
         # Remove white space of player_name and concatentate with player_tag
         self._puuid = None
-        self.player_name = player_name
+        self._player_name = player_name
         self.player_tag = player_tag
 
         # Basic error checking
         assert region.lower() in ["americas", "asia", "esports", "europe"]
         self.region = region.lower()
+        self.detailed_region = detailed_region.lower()
 
         # Set puuid, an unique identifier
         self.init_puuid()
@@ -22,6 +27,10 @@ class Player:
     @property
     def puuid(self):
         return self._puuid
+
+    @property
+    def player_name(self):
+        return self._player_name
 
     def init_puuid(self):
         """Get puuid of player based on their ingame name and tag
@@ -42,32 +51,55 @@ class Player:
         player_info = resp.json()
         self._puuid = player_info["puuid"]
 
-    def get_match_ids(self):
-        api_url = (
-            "https://"
-            + mass_region
-            + ".api.riotgames.com/lol/match/v5/matches/by-puuid/"
-            + puuid
-            + "/ids?start=0&count=20"
-            + "&api_key="
-            + api_key
-        )
 
-        print(api_url)
+class PlayerData(Player):
 
-        resp = requests.get(api_url)
-        match_ids = resp.json()
-        return match_ids
+    def __init__(
+        self, player_name: str, player_tag: str, region: str, detailed_region: str
+    ):
+        super().__init__(player_name, player_tag, region, detailed_region)
 
-    def _concat_name_and_tag(self) -> str:
-        """Return a concatenated name
-        Ex. Hide on bush#KR1 -> Hide%20on%20bush/KR1
-        """
-        return
+    @staticmethod
+    def opgg_extract_player(self):
+        """Extract the exact player (player_name#player_tag) from oppg api"""
+        # If error happens just remove staticmethod
+        opgg = OPGG(None, Region.KR)
+
+        summoner: Summoner
+        for summoner in opgg.search(["Peyz#KR11"], Region.KR):
+            print(summoner)
+        print(len(opgg.search(["Peyz#KR1"], Region.KR)))
+        # exact_summoner = list(
+        #     filter(
+        #         lambda x: x.acct_id == self.puuid, opgg.search(["JUGKING"], Region.KR)
+        #     )
+        # )
+
+    # def get_match_ids(self):
+    #     api_url = (
+    #         "https://"
+    #         + mass_region
+    #         + ".api.riotgames.com/lol/match/v5/matches/by-puuid/"
+    #         + puuid
+    #         + "/ids?start=0&count=20"
+    #         + "&api_key="
+    #         + api_key
+    #     )
+    #
+    #     print(api_url)
+    #
+    #     resp = requests.get(api_url)
+    #     match_ids = resp.json()
+    #     return match_ids
 
 
-faker = Player("Hide on bush", "KR1", "Asia")
+faker = Player("Hide on bush", "KR1", "Asia", "kr")
 print(faker.puuid)
 
-canyon = Player("JUGKING", "KR1", "Asia")
+canyon = Player("Peyz", "KR11", "Asia", "kr")
+
 print(canyon.puuid)
+canyon_data = PlayerData(
+    canyon.player_name, canyon.player_tag, canyon.region, canyon.detailed_region
+)
+canyon_data.opgg_extract_player(canyon_data)
